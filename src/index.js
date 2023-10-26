1f97ec89626f26258ef3568ff99fa05e55d8935b
agungsdas <78905860+agungsdas@users.noreply.github.com>
2023-10-26 09:56:21 +0700

    feat: implement cicd (#1)
    Co-authored-by: Agung <agung.dasritara@mitrakeluarga.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+const basePath = process.env.BASE_PATH || '/'
+
+const routers = express.Router()
+routers.get('/', (req, res) => {
   res.send('Hello World!!')
 })
 
+app.use(basePath, routers);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
